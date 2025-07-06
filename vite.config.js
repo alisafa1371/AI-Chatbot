d89e6fe4d73b022e13b8a10ad6623cb2dabd9cf6
alisafa1371 <ali_safaeyan@yahoo.com>
2025-07-06 15:09:08 +0330

    v.5

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    env: {
+      VITE_OPENROUTER_KEY: "$VITE_OPENROUTER_KEY",
+    },
+  },
 });
