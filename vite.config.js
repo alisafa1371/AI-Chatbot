7e25fe6165b6de8f6ddad7321a6b14cee8c409f5
alisafa1371 <ali_safaeyan@yahoo.com>
2025-07-13 09:02:10 +0330

    changing package.json for deploying gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  build: {
-    env: {
-      VITE_OPENROUTER_KEY: "$VITE_OPENROUTER_KEY",
-    },
-  },
+  base: "/AI-Chatbot",
 });
